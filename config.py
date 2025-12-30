53a5344ec82929aac8e68e5c73d54031f5ec87d1
devdjambulat <djambulatm925@gmail.com>
2025-12-30 14:28:26 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,9 @@
-BOT_TOKEN = "8261613695:AAHDpiCiGaxjghXwHRT-xPC960SZfhOYsKg"
-ADMIN_IDS = ["8240413089"]  # сюда твой Telegram ID
-DB_PATH = "database.db"
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+# ADMIN_IDS берём из переменной окружения и превращаем в список чисел
+ADMIN_IDS = list(map(int, os.getenv("ADMIN_IDS").split(",")))
+
+DB_PATH = os.getenv("DB_PATH", "database.db")
+
