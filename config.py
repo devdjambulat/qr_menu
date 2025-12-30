32d7f8e7836fa36294b2903c1ff49c0f48788b38
devdjambulat <djambulatm925@gmail.com>
2025-12-30 14:16:21 +0500

    Change ADMIN_IDS to string format

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 BOT_TOKEN = "8261613695:AAHDpiCiGaxjghXwHRT-xPC960SZfhOYsKg"
-ADMIN_IDS = [8240413089]  # сюда твой Telegram ID
+ADMIN_IDS = ["8240413089"]  # сюда твой Telegram ID
 DB_PATH = "database.db"
